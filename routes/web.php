50e287355155f6a6402a3d96f4d067952e04290e
Nicolas Sawczuk <nico.290698@gmail.com>
2020-05-16 12:54:01 -0300

    Cambio de parametro

--- routes/web.php
+++ routes/web.php
@@ -20,25 +20,25 @@ Route::get('/', function () {
 Route::get('modulos', 'ModuloController@index')->name('modulos.index');
 Route::get('modulos/create', 'ModuloController@create')->name('modulos.create');
 Route::post('modulos', 'ModuloController@store')->name('modulos.store');
-Route::get('modulos/{id}/edit', 'ModuloController@edit')->name('modulos.edit') ;
-Route::put('modulos/{id}', 'ModuloController@update')->name('modulos.update') ;
-Route::delete('modulos/delete/{id}', 'ModuloController@delete')->name('modulos.delete');
+Route::get('modulos/{modulo}/edit', 'ModuloController@edit')->name('modulos.edit') ;
+Route::put('modulos/{modulo}', 'ModuloController@update')->name('modulos.update') ;
+Route::delete('modulos/delete/{modulo}', 'ModuloController@delete')->name('modulos.delete');
 
 //Temas
 Route::get('temas', 'TemaController@index')->name('temas.index');
 Route::get('temas/create', 'TemaController@create')->name('temas.create');
 Route::post('temas', 'TemaController@store')->name('temas.store');
-Route::get('temas/{id}/edit', 'TemaController@edit')->name('temas.edit') ;
-Route::put('temas/{id}', 'TemaController@update')->name('temas.update') ;
-Route::delete('temas/delete/{id}', 'TemaController@delete')->name('temas.delete');
+Route::get('temas/{tema}/edit', 'TemaController@edit')->name('temas.edit') ;
+Route::put('temas/{tema}', 'TemaController@update')->name('temas.update') ;
+Route::delete('temas/delete/{tema}', 'TemaController@delete')->name('temas.delete');
 
 //Palabras
 Route::get('palabras', 'PalabraController@index')->name('palabras.index');
 Route::get('palabras/create', 'PalabraController@create')->name('palabras.create');
 Route::post('palabras', 'PalabraController@store')->name('palabras.store');
-Route::get('palabras/{id}/edit', 'PalabraController@edit')->name('palabras.edit') ;
-Route::put('palabras/{id}', 'PalabraController@update')->name('palabras.update') ;
-Route::delete('palabras/delete/{id}', 'PalabraController@delete')->name('palabras.delete');
+Route::get('palabras/{palabra}/edit', 'PalabraController@edit')->name('palabras.edit') ;
+Route::put('palabras/{palabra}', 'PalabraController@update')->name('palabras.update') ;
+Route::delete('palabras/delete/{palabra}', 'PalabraController@delete')->name('palabras.delete');
 
 
 Auth::routes();
