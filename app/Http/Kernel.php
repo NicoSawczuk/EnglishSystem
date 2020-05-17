286e205fd16044a7f81a564224123b157c68b674
Agus <agus.britez@hotmail.com>
2020-05-17 13:34:04 -0300

    Routas shinobbi

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,10 +63,10 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'role' => \Caffeinated\Shinobi\Middleware\UserHasRole::class,
-        'permission' => \Caffeinated\Shinobi\Middleware\UserHasPermission::class,
-        // 'has.role' => \Caffeinated\Shinobi\Middleware\UserHasRole::class,
-        // 'has.permission' => \Caffeinated\Shinobi\Middleware\UserHasPermission::class,
+        // 'role' => \Caffeinated\Shinobi\Middleware\UserHasRole::class,
+        // 'permission' => \Caffeinated\Shinobi\Middleware\UserHasPermission::class,
+        'has.role' => \Caffeinated\Shinobi\Middleware\UserHasRole::class,
+        'has.permission' => \Caffeinated\Shinobi\Middleware\UserHasPermission::class,
 
     ];
 
