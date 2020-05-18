1434319c25a03d62d19b4a5f6c5fed85846c9d30
Nicolas Sawczuk <nico.290698@gmail.com>
2020-05-18 18:15:32 -0300

    Traduccion de login, registro y nabvar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,11 +84,11 @@
                         <!-- Authentication Links -->
                         @guest
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav-link" href="{{ route('login') }}">Ingreso</a>
                         </li>
                         @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            <a class="nav-link" href="{{ route('register') }}">Registro</a>
                         </li>
                         @endif
                         @else
@@ -101,7 +101,7 @@
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                                    Cerrar sesión
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST"
