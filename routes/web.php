9fc9486a7c082ccfeebee9b4ea6ce2a51e05308a
Nicolas Sawczuk <nico.290698@gmail.com>
2020-05-19 09:29:48 -0300

    Arreglo de permisos en rutas

--- routes/web.php
+++ routes/web.php
@@ -26,28 +26,28 @@ Route::get('/home/ajax_practica_temas', 'HomeController@cargarCard')->name('home
 
 //Modulos
 Route::get('modulos', 'ModuloController@index')->name('modulos.index')->middleware('can:abm');
-Route::get('modulos/create', 'ModuloController@create')->name('modulos.create');
-Route::post('modulos', 'ModuloController@store')->name('modulos.store');
-Route::get('modulos/{modulo}/edit', 'ModuloController@edit')->name('modulos.edit');
-Route::put('modulos/{modulo}', 'ModuloController@update')->name('modulos.update');
-Route::delete('modulos/delete/{modulo}', 'ModuloController@delete')->name('modulos.delete');
+Route::get('modulos/create', 'ModuloController@create')->name('modulos.create')->middleware('can:abm');;
+Route::post('modulos', 'ModuloController@store')->name('modulos.store')->middleware('can:abm');;
+Route::get('modulos/{modulo}/edit', 'ModuloController@edit')->name('modulos.edit')->middleware('can:abm');;
+Route::put('modulos/{modulo}', 'ModuloController@update')->name('modulos.update')->middleware('can:abm');;
+Route::delete('modulos/delete/{modulo}', 'ModuloController@delete')->name('modulos.delete')->middleware('can:abm');;
 
 //Temas
 Route::get('temas', 'TemaController@index')->name('temas.index')->middleware('can:abm');
-Route::get('temas/create', 'TemaController@create')->name('temas.create');
-Route::post('temas', 'TemaController@store')->name('temas.store');
-Route::get('temas/{tema}/edit', 'TemaController@edit')->name('temas.edit');
-Route::put('temas/{tema}', 'TemaController@update')->name('temas.update');
-Route::delete('temas/delete/{tema}', 'TemaController@delete')->name('temas.delete');
+Route::get('temas/create', 'TemaController@create')->name('temas.create')->middleware('can:abm');;
+Route::post('temas', 'TemaController@store')->name('temas.store')->middleware('can:abm');;
+Route::get('temas/{tema}/edit', 'TemaController@edit')->name('temas.edit')->middleware('can:abm');;
+Route::put('temas/{tema}', 'TemaController@update')->name('temas.update')->middleware('can:abm');;
+Route::delete('temas/delete/{tema}', 'TemaController@delete')->name('temas.delete')->middleware('can:abm');;
 
 //Palabras
 Route::get('palabras', 'PalabraController@index')->name('palabras.index')->middleware('can:abm');
-Route::get('palabras/create', 'PalabraController@create')->name('palabras.create');
-Route::post('palabras', 'PalabraController@store')->name('palabras.store');
-Route::get('palabras/{palabra}/edit', 'PalabraController@edit')->name('palabras.edit');
-Route::put('palabras/{palabra}', 'PalabraController@update')->name('palabras.update');
-Route::delete('palabras/delete/{palabra}', 'PalabraController@delete')->name('palabras.delete');
-Route::get('palabras/getTemas/{modulo}', 'PalabraController@getTemas')->name('palabras.getTemas');
+Route::get('palabras/create', 'PalabraController@create')->name('palabras.create')->middleware('can:abm');;
+Route::post('palabras', 'PalabraController@store')->name('palabras.store')->middleware('can:abm');;
+Route::get('palabras/{palabra}/edit', 'PalabraController@edit')->name('palabras.edit')->middleware('can:abm');;
+Route::put('palabras/{palabra}', 'PalabraController@update')->name('palabras.update')->middleware('can:abm');;
+Route::delete('palabras/delete/{palabra}', 'PalabraController@delete')->name('palabras.delete')->middleware('can:abm');;
+Route::get('palabras/getTemas/{modulo}', 'PalabraController@getTemas')->name('palabras.getTemas')->middleware('can:abm');;
 
 
 Auth::routes();
